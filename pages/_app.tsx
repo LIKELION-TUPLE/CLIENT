4ad1280e5aeec78fb19e6757ac7266da2d1addc9
KwonET <dlxl3652zz@gmail.com>
2023-09-11 18:53:55 +0900

    [ init ] : 초기세팅

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,25 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RecoilRoot } from 'recoil';
+import GlobalStyle from '@src/styles/globalStyle';
+import { theme } from '../src/styles/theme';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import React from 'react';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = React.useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </RecoilRoot>
+    </QueryClientProvider>
+  );
 }
+
+export default MyApp;
