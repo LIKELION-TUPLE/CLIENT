4bcc633ecd72983d6cc83139c186e819f16fc302
KwonET <dlxl3652zz@gmail.com>
2023-11-01 15:38:07 +0900

    [ feat ] : 날짜 별 콘텐츠 추가

--- components/TupleCalendar.tsx
+++ components/TupleCalendar.tsx
@@ -1,30 +1,55 @@
 import React, { useEffect, useState } from 'react';
 import Calendar from 'react-calendar';
 import * as moment from 'moment';
-
-// import 'react-calendar/dist/Calendar.css'; // css import
+import 'moment/locale/ko';
+import { dayList } from 'data/dummy';
+import styled from 'styled-components';
 
 type ValuePiece = Date | null;
 type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 const TupleCalendar = () => {
-  const [date, setDate] = useState<Value>(new Date());
-
+  const [date, setDate] = useState<Value>(new Date()); //선택된 날짜
+  const addContent = ({ date }: any) => {
+    const contents = [];
+    if (dayList.find((day) => day === moment(date).format('YYYY-MM-DD'))) {
+      contents.push(<div className="dot"></div>);
+    }
+    return <div>{contents}</div>;
+  };
   useEffect(() => {
-    console.log(date);
+    // console.log(contents);
   }, []);
   return (
-    <div>
+    <Wrapper>
+      <TitleSection>
+        <SubTitle>김인강 선생님, 어서오세요!</SubTitle>
+        <Title>과외 일정</Title>
+      </TitleSection>
       <Calendar
         locale="ko"
         onChange={setDate}
-        formatDay={(locale, date) => moment(date).format('DD')}
-        navigationLabel={null}
-        showNeighboringMonth={false}
+        formatDay={(locale, date) => moment(date).format('D')} // 0일
+        next2Label={null} //년 네비게이션 표시 여부
+        prev2Label={null} //년 네비게이션 표시 여부
+        showNeighboringMonth={false} //최근 달 일자 표시 여부
+        tileContent={addContent}
         value={date}
       />
-    </div>
+      <ClassSection>
+        <DateTitle>{moment(date).format('MM월 DD일 (dd)')}</DateTitle>
+        <SubTitle>오늘의 수업</SubTitle>
+        <ClassContainer></ClassContainer>
+      </ClassSection>
+    </Wrapper>
   );
 };
 
 export default TupleCalendar;
+const Wrapper = styled.div``;
+const TitleSection = styled.div``;
+const Title = styled.h1``;
+const SubTitle = styled.h2``;
+const ClassSection = styled.section``;
+const DateTitle = styled.h1``;
+const ClassContainer = styled.div``;
