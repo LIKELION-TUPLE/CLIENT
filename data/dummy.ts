ac5367a189c8b192f7f3d5159c34906dd31c1437
Kinngife <qu12170@naver.com>
2023-11-20 08:51:05 +0900

    [ feat ] : 더미 데이터 삭제

--- data/dummy.ts
+++ data/dummy.ts
@@ -1,13 +1,4 @@
 export const dayList = ['2023-10-31', '2023-11-09', '2023-11-23'];
-export const dummyUsers = {
-  users: [
-    { key: 1, id: 'rkdalstjr' },
-    { key: 2, id: 'rnjsqhal' },
-    { key: 3, id: 'rhdbwls' },
-    { key: 4, id: 'dhdmsxor' },
-    { key: 5, id: 'dlawjddus' },
-  ],
-};
 export const studentList = [
   { color: '#FFAFA3', school: '서강고 2학년', name: '김기철', subject: '수학', time: '19:00 ~ 21:00', turn: '1회차' },
   { color: '#3DCC0A', school: '해피고 3학년', name: '강민석', subject: '가정', time: '19:00 ~ 21:00', turn: '3회차' },
