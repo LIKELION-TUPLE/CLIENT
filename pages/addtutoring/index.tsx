a3974752332837ce66186956d0271e7fdbc4ed85
Kinngife <qu12170@naver.com>
2023-11-20 08:52:22 +0900

    [ feat ] : pages, component 이름 수정

--- pages/addtutoring/index.tsx
+++
@@ -1,8 +0,0 @@
-import AddTutoring from 'components/tutoring/AddTutoring';
-import React from 'react';
-
-const index = () => {
-  return <AddTutoring />;
-};
-
-export default index;
--- pages/completeaddtutoring/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import CompleteAddTutoring from 'components/tutoring/CompleteAddTutoring';
-
-const index = () => {
-  return <CompleteAddTutoring />;
-};
-
-export default index;
